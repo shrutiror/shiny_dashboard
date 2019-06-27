a96cf8f6bef49fd3ab30d1850ccff7371e1d1b80
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-06-27 19:45:17 +0530

    update

--- 1st_dashboard_implement/server.R
+++ 1st_dashboard_implement/server.R
@@ -4,7 +4,11 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
-    output$histogram<-renderPlot(hist(faithful$eruptions,breaks=input$bins))
+    output$histogram<-renderPlot({
+        
+        hist(faithful$eruptions,breaks=input$bins)
+        
+        })
     
 }
 )
