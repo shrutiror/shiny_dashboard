2fee5bb5ed9ef7fa9264998bf9f3bb877b980d21
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-07-01 13:10:23 +0530

    Update ui.R

--- multiple_controls/ui.R
+++ multiple_controls/ui.R
@@ -12,20 +12,26 @@ shinyUI(
          dashboardSidebar(
             sidebarMenu(
                 sidebarSearchForm("SearchText","buttonSearch","Search"),
-                sliderInput("bins","no. of breaks",1,100,50),
                 menuItem("Dashboard",tabName = "Dashboard"),
                 menuSubItem("Dashboard Sales",tabName ="sales"),
                 menuSubItem("Dashboard Finanace",tabName ="finance"),
                 menuItem("Detailed analysis"),
-                menuItem("Raw data"),
-                textInput("text","search opportunities",value="12345")
+                menuItem("Raw data")
             )
+            
         ),
         dashboardBody(
             tabItems(
                 tabItem( tabName = "Dashboard",
                          fluidRow(
-                             box( title=h3("Histogram of faithful"), status="warning",solidHeader=T,plotOutput("histogram")))
+                        tabBox(
+                        tabPanel( title=h3("Histogram of faithful"),
+                        status="warning",solidHeader=T,background="aqua",plotOutput("histogram")),
+                        tabPanel( title=h3("controls for histogram"), 
+                        status="danger",solidHeader=T,background="green",
+                        sliderInput("bins","no. of breaks",1,100,50), 
+                        textInput("text","search opportunities",value="12345")))
+                         )
                 ),
                 tabItem(tabName ="finance",h1("Finance dashboard")),
                 tabItem(tabName ="sales",h1("sales dashboard"))
